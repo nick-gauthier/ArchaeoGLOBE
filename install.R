b09b26706204d9bb9edd54e7db94f77fec942506
Ben Marwick <benmarwick@hotmail.com>
2019-01-17 15:13:02 -0800

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 # Installs things for binder
 system("apt-get install libudunits2-dev -y")
 install.packages(c('tidyverse', 'sf', 'mgcv', 'caTools', 'bitops'))
-devtools::install_github('thomasp85/patchwork')
+devtools::install_github(c('thomasp85/patchwork', 'centerforopenscience/osfr')) 
 devtools::install(".")
