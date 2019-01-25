d9493c7c45dc24fd148d0fc70daaf9177d2e0b2a
Ben Marwick <benmarwick@hotmail.com>
2019-01-24 16:44:37 -0800

    read in data directly from dataverse

--- install.R
+++ install.R
@@ -1,6 +1,6 @@
 # Installs things for binder
 system("apt-get install libudunits2-dev -y")
-install.packages(c('tidyverse', 'sf', 'mgcv', 'caTools', 'bitops'))
+install.packages(c('tidyverse', 'sf', 'mgcv', 'caTools', 'bitops', 'dataverse'))
 devtools::install_github('thomasp85/patchwork')
 devtools::install_github('centerforopenscience/osfr')
 devtools::install(".")
