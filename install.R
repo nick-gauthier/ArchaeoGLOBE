124c08e9a8835d81b63b093d66e2be571345a3c1
nick-gauthier <nicolas.gauthier@asu.edu>
2019-08-23 19:31:40 -0700

    holepunch

--- install.R
+++
@@ -1,6 +0,0 @@
-# Installs things for binder
-system("apt-get install libudunits2-dev -y")
-install.packages(c('raster', 'tidyverse', 'sf', 'mgcv', 'dataverse', 'git2r'))
-devtools::install_github('thomasp85/patchwork')
-devtools::install_github('centerforopenscience/osfr')
-devtools::install(".")
